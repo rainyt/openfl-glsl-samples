6b515b02bc430dfda63cedd3f0344ac8598cb7ae
yutian.liu <939029835@qq.com>
2021-05-16 23:03:01 +0800

    新增光波效果

--- Source/Main.hx
+++ Source/Main.hx
@@ -14,7 +14,7 @@ class Main extends Sprite {
 		this.addChild(bitmap);
 
 		// 从这里更换GLSL目标
-		bitmap.shader = new glsl.LightPointRotation();
+		bitmap.shader = new glsl.LightEffect();
 		bitmap.addEventListener(Event.ENTER_FRAME, function(e) {
 			bitmap.invalidate();
 		});
---
+++ Source/glsl/LightEffect.hx
@@ -0,0 +1,42 @@
+package glsl;
+
+import openfl.Lib;
+import VectorMath;
+
+@:debug
+class LightEffect extends OpenFLShader {
+	@:uniform public var time:Float;
+
+	@:define("r openfl_TextureSize.xy")
+	override function fragment() {
+		super.fragment();
+
+		// 设置为黑色
+		color = vec4(0, 0, 0, 1);
+
+		var c:Vec3 = vec3(0, 0, 0);
+		var l:Float = time;
+		var z:Float = time;
+		for (i in 0...3) {
+			var uv:Vec2 = gl_openfl_TextureCoordv;
+			var p:Vec2 = uv;
+			p -= .5;
+			p.x *= r.x / r.y;
+			z += 0.7;
+			l = length(p);
+			uv += p / l * (sin(z) + 1.) * abs(sin(l * 9. - z * 2.));
+			c[i] = .01 / length(abs(mod(uv, 1.) - .5));
+		}
+		gl_FragColor = vec4(c / l, time);
+	}
+
+	public function new() {
+		super();
+		this.u_time.value = [0];
+		Lib.setInterval(update, 0);
+	}
+
+	public function update():Void {
+		this.u_time.value[0] += 1 / 600;
+	}
+}
